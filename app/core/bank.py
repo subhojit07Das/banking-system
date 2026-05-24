6c7e10f2cf61ba8ba535a05941928ddbaaaf61fb
Subhojit Das <subhojit05das@gmail.com>
2026-05-24 19:19:50 +0530

    Phase 2: Bank class with create, get and list accounts

---
+++ app/core/bank.py
@@ -0,0 +1,18 @@
+from app.core.account import Account
+
+class Bank:
+    def __init__(self):
+        self.accounts = {}
+
+    def create_account(self, owner, initial_balance):
+        acc = Account(owner, initial_balance)
+        self.accounts[acc.id] = acc
+        return acc;
+
+    def get_account(self, account_id):
+        if account_id in self.accounts:
+            return self.accounts[account_id]
+        return "Account not found"
+    
+    def list_account(self):
+        return self.accounts.values()
